3d8d4a5c851d42300f4651e192285c3623270b69
robson arruda <robsonshockwavegithub@gmail.com>
2025-08-23 22:09:19 -0300

    criando controller e rotas para buscar serviços, serviço específico e deletar serviço

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -20,6 +20,54 @@ const serviceController = {
       res.status(500).json({ error: 'Erro ao criar o serviço.' });
     }
   },
+
+  getAll: async (req, res) => {
+    try {
+      const services = await ServiceModel.find();
+      res.status(200).json(services);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: 'Erro ao buscar os serviços.' });
+    }
+  },
+
+  get: async (req, res) => {
+    try {
+      const id = req.params.id;
+
+      const service = await ServiceModel.findById(id);
+
+      if (!service) {
+        return res.status(404).json({ error: 'Serviço não encontrado.' });
+      }
+
+      res.status(200).json(service);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: 'Erro ao buscar o serviço.' });
+    }
+  },
+
+  delete: async (req, res) => {
+    try {
+      const id = req.params.id;
+
+      const service = await ServiceModel.findById(id);
+
+      if (!service) {
+        return res.status(404).json({ error: 'Serviço não encontrado.' });
+      }
+
+      const deletedService = await ServiceModel.findByIdAndDelete(id);
+
+      res
+        .status(200)
+        .json({ message: 'Serviço excluido com sucesso!', deletedService });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: 'Erro ao excluir o serviço.' });
+    }
+  },
 };
 
 module.exports = serviceController;
